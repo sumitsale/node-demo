ca0f1a1edc6365e69a5aa0d6531932e179af2396
Sumit Sale <sumits2@citiustech.com>
2016-08-22 15:16:48 +0530

    theme integration and public and src folder created

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var app = express();
 
 var port = 5000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
 app.get('/', function(req, res){
 	res.send('Hello World');
 });
