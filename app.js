2d2ef95acfc89b9dc5e1965987dc98c9c2ec89db
Sumit Sale <sumits2@citiustech.com>
2016-08-22 13:46:38 +0530

    Route added

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var app = express();
 
 var port = 5000;
 
+app.get('/', function(req, res){
+	res.send('Hello World');
+});
+
+app.get('/books', function(req, res){
+	res.send('Hello Books');
+});
+
 app.listen(port, function(err){
 
 	console.log("running server on port " + port);
