b535af53dd229d18cb761d54800e949894d5e141
Sumit Sale <sumits2@citiustech.com>
2016-08-24 10:34:06 +0530

    gulp code commit

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-var express = require("express");
+var express = require('express');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 	res.send('Hello World');
 });
 
-app.get('/books', function(req, res){
+app.get('/books', function (req, res) {
 	res.send('Hello Books');
 });
 
-app.listen(port, function(err){
+app.listen(port, function (err) {
 
-	console.log("running server on port " + port);
+	console.log('running server on port ' + port);
 
 });
 
